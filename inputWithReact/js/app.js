10486aca862f5006c79308dad2c167427d6e9fe2
Babar shair <babarshair97@gmail.com>
2017-10-28 15:08:29 +0500

    Using Class

--- inputWithReact/js/app.js
+++ inputWithReact/js/app.js
@@ -1,14 +1,14 @@
-const container =  document.getElementById('container')
+// const container =  document.getElementById('container')
 
 
-let input = React.createElement('input', {type : "text", className : "form-control col-5"})
-let button = React.createElement('button',{type : "submit", className : "btn btn-primary col-5  "}, "Click Me")
-let mainDiv = React.createElement("div", null , input, button)
+// let input = React.createElement('input', {type : "text", className : "form-control col-5"})
+// let button = React.createElement('button',{type : "submit", className : "btn btn-primary col-5  "}, "Click Me")
+// let mainDiv = React.createElement("div", null , input, button)
 
-ReactDOM.render(
-    mainDiv,
-    container
-)
+// ReactDOM.render(
+//     mainDiv,
+//     container
+// )
 // ReactDOM.render(
 //     React.createElement('h1', null, 'hello world'),
 //     document.getElementById('container')
@@ -19,6 +19,30 @@ ReactDOM.render(
 //         document.getElementById('container')
 //     );
 
+/***************    CustomeClass/ CustomeHeadings   *************************/
+
+class babar extends React.Component{
+    render(){
+        return React.createElement('h1',{style : this.props.style}, this.props.name);
+    }
+}
+class zeshan extends React.Component{
+    render(){
+        return React.createElement('h2',{style : this.props.style}, this.props.name);
+    }
+}
+class fahad extends React.Component{
+    render(){
+        return React.createElement('h3',{style : this.props.style}, this.props.name);
+    }
+}
+var myName = React.createElement(babar,{style : {color : "red", border : '2px solid black'}, name : 'babar'})
+var myName2 = React.createElement(zeshan,{style : {color : "blue", border : '2px solid orange'}, name : 'Zeshan'})
+var myName3 = React.createElement(fahad,{style : {color : "green", border : '2px solid pink'}, name : 'Fahad'})
+ReactDOM.render(
+    React.createElement('div' , {className : 'container'}, myName, myName2, myName3),
+    document.getElementById('container')
+)
 // class CustomeLink extends React.Component {
 //     render() {
 //         return React.createElement('a', { href: this.props.link }, this.props.linkText);
